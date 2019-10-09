7d34e283a0c8e2e1fa001a4bcf8ac733d858aadf
Aress <a.ress@live.ru>
2019-10-09 20:45:14 +0300

    added soup

--- slurryparser.py
+++ slurryparser.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
+import urllib.request
+from bs4 import BeautifulSoup
+
+def get_html(url):
+    response = urllib.request.urlopen(url)
+    return response.read()
+
+def parser(html):
+    soup = BeautifulSoup(html)
 
 def main():
+    print(get_html('https://zenmod.ru/'))
+
+if __name__ == '__main__':
+    main()
