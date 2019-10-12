27e4c531552cc70d52e5d8c6d5cd4de847b1cd64
Aress <a.ress@live.ru>
2019-10-12 21:58:40 +0300

    first part of the parser

--- slurryparser.py
+++ slurryparser.py
@@ -6,11 +6,20 @@ def get_html(url):
     response = urllib.request.urlopen(url)
     return response.read()
 
-def parser(html):
-    soup = BeautifulSoup(html)
+def parse(html):
+    soup = BeautifulSoup(html,"html.parser")
+    grid = soup.findAll('', class_="dz_product-item-title")
+    titles_list = []
+    for row in grid:
+        f =row.get('title')
+        titles_list.append(f)
+    print(titles_list)
+    #title=grid.find('h3')
+    print(grid)
 
 def main():
-    print(get_html('https://zenmod.ru/'))
+    parse(get_html('https://vapeliga.ru/zhidkosti_usa/'))
+    print(parse)
 
 if __name__ == '__main__':
     main()
