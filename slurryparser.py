32c6231cd22454d7fd7a2bcc8b07a62db9007ba5
a-res <a.ress@live.ru>
2019-10-15 22:55:51 +0300

    added england and malaysia

--- slurryparser.py
+++ slurryparser.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+gi#!/usr/bin/env python3
 import urllib.request
 import re
 from bs4 import BeautifulSoup
@@ -20,6 +20,7 @@ def parse(html):
 
 def main():
     adress_def = 'https://vapeliga.ru/'
+
     for p in range(31):
       adress=adress_def+('zhidkosti_usa/filter/clear/apply/?PAGEN_1='+str(p))
       parse(get_html(adress))
@@ -29,6 +30,18 @@ def main():
           adress = adress_def + ('zhidkosti_rossiya/filter/clear/apply/?PAGEN_1=' + str(k))
           parse(get_html(adress))
           print(adress)
+
+          for m in range(6):
+              adress = adress_def + ('/zhidkosti_malaysia/filter/clear/apply/?PAGEN_1=' + str(m))
+              parse(get_html(adress))
+              print(adress)
+
+              for e in range(3):
+                  adress = adress_def + ('/zhidkosti_england/filter/clear/apply/?PAGEN_1=' + str(e))
+                  parse(get_html(adress))
+                  print(adress)
+
+
 
       #print(parse)
 if __name__ == '__main__':
